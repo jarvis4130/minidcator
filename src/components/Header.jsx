a02d887d4fdac244728a3527dc78ab6f745b90c0
jarvis4130 <atharvaadam101010@gmail.com>
2024-04-11 19:59:45 +0530

    2342423

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,26 +7,26 @@ const Header = () => {
     <header className="bg-blue-500 text-white py-4 px-4 sm:px-20 flex flex-row justify-between">
       <div className="container">
         <div
-          className="w-20"
+          className="w-20 flex flex-col gap-2"
           onClick={() => {
             navigate("/cab");
           }}
         >
           <img src="/images/m-indicator.jpg" alt="this is an image" />
-          <p>Best Way</p>
+          <p className="bg-gray-600" >Best Way</p>
         </div>
       </div>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center gap-5">
         <div className="w-10">
           <div>
             <img src="/images/placeholder.jpg" alt="" className="rounded-full" />
           </div>
         </div>
         <div
-          className="text-xl font-semibold cursor-pointer"
+          className="text-xl font-semibold cursor-pointer "
           onClick={() => navigate("/")}
         >
-         <p>User</p>
+         <p className="bg-gray-600">User</p>
         </div>
       </div>
     </header>
