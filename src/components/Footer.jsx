42558d0e6a5238161ef1149032ef63956c878a28
jarvis4130 <atharvaadam101010@gmail.com>
2024-04-11 15:21:40 +0530

    233

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-blue-500 text-white py-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 Cab Booking App. All rights reserved.</p>
+        <p>&copy; 2024 Transport Booking App. All rights reserved.</p>
       </div>
     </footer>
   );
